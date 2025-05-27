f7bafc29640514cf312e2d6dc408c131e6737df5
bryn-git <bryan.akmal35@gmail.com>
2025-05-27 09:01:11 +0700

    update:linkapi

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   base: '/',
   server: {
     host: "0.0.0.0",
-    port: 3000,
+    port: 8082,
     proxy: {
       '/api': {
         target: "http://localhost:8080",
